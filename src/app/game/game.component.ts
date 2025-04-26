8b84252aedf4feea932c0bf7a0ba9bc71eae859e
AlkanTetik <alkan.tetik.at@gmail.com>
2025-04-26 17:19:43 +0200

    pushed all the stuff

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game | undefined;
+  constructor(public dialog: MatDialog) {
+    this.newGame();
+    console.log(this.game);
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(name => {
+      if (name && name.length > 0) {
+        this.game?.players.push(name);
+      }
+    });
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+  pickCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game?.stack.pop() || '';
+      this.pickCardAnimation = true;
+      console.log(this.game?.playedCards);
+
+      if (this.game) {
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      }
+      setTimeout(() => {
+        this.game?.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  }
+}
